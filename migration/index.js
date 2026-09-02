48305e9213c40647e9e07fc1de5df4849373e620
mehdi <mehdimoazemi.t@gmail.com>
2026-09-02 16:19:13 +0330

    refactor(db): remove unnecessary callbacks from fs.readFileSync

--- migration/index.js
+++ migration/index.js
@@ -6,24 +6,13 @@ const migrate = async () => {
 
     const connection = await db.getConnection()
 
-    const createTableUsersSql = fs.readFileSync(path.resolve(__dirname, "users-ddl.sql"), "utf8", (err, data) => {
-        if (err) throw err
-        return data
-    })
+    const createTableUsersSql = fs.readFileSync(path.resolve(__dirname, "users-ddl.sql"), "utf8")
 
-    const createTableArticlesSql = fs.readFileSync(path.resolve(__dirname, "articles-ddl.sql"), "utf8", (err, data) => {
-        if (err) throw err
-        return data
-    })
-    const createTableTagsSql = fs.readFileSync(path.resolve(__dirname, "tags-ddl.sql"), "utf8", (err, data) => {
-        if (err) throw err
-        return data
-    })
+    const createTableArticlesSql = fs.readFileSync(path.resolve(__dirname, "articles-ddl.sql"), "utf8")
+    
+    const createTableTagsSql = fs.readFileSync(path.resolve(__dirname, "tags-ddl.sql"), "utf8",)
 
-    const createTableArticleTagSql = fs.readFileSync(path.resolve(__dirname, "article_tag-ddl.sql"), "utf8", (err, data) => {
-        if (err) throw err
-        return data
-    })
+    const createTableArticleTagSql = fs.readFileSync(path.resolve(__dirname, "article_tag-ddl.sql"), "utf8")
 
 
     connection.beginTransaction()
