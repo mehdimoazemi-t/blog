5e3ed59cc84b7dbe097f604b9f6ccc9859d5bdd3
mehdi <mehdimoazemi.t@gmail.com>
2026-09-09 16:42:00 +0330

    feat: add article controller with tag linking and tag-based article retrieval

---
+++ controller/articles.js
@@ -0,0 +1,69 @@
+const Article = require("../repository/articles");
+const Tags = require("../repository/tags");
+
+exports.add = async (req, res, next) => {
+    
+    try {
+        const { title, content, slug, tags } = req.body
+
+     
+        const article = await Article.create({
+            title,
+            content,
+            slug,
+            cover: req.file.filename,
+            author_id: req.user.id
+        })
+
+        await tags?.forEach(async tagId => {
+            await Article.addTag(article.id, tagId)
+        });
+
+        return res.status(201).json({ message: "Article create successfully" })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.findTagArticle = async (req, res, next) => {
+    try {
+
+        const { tagName } = req.params
+
+        const tag = await Tags.findByName(tagName);
+
+        const articles = await Article.findArticlesByTag(tag.id)
+
+        return res.status(200).json(articles)
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+exports.getAll = async (req, res, next) => {
+    // Code ...
+}
+
+exports.delete = async (req, res, next) => {
+    // Code ...
+}
+
+exports.getBySlug = async (req, res, next) => {
+
+    const { slug } = req.params
+
+    const article = await Article.findOne(slug);
+
+    if (!article) {
+        return res.status(404).json({
+            message: "Article Not Found"
+        })
+    }
+
+
+    return res.status(200).json(article)
+
+}
